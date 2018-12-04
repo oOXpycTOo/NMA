4a438baeb119bb18f772bfa4c4ccaada49961493
oOXpycTOo <vladkaz1999@gmail.com>
2018-12-04 11:38:38 +0300

    Added some matrix utils

--- linalg_utils.py
+++ linalg_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def back_substitution(A, b, start_from="bottom"):
     if(start_from == "top"):
         return substitute_from_top(A, b)
@@ -17,3 +19,14 @@ def substitute_from_bottom(A, b):
     for i in range(n, zero, -1):
         x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i,i]
     return x
+
+def calculate_discrepancy(A, b, x):
+    return np.dot(A, x) - b
+
+def is_positive_defined(A):
+    return np.all(np.linalg.eigvals(A) > 0)
+
+def is_symmetric(A):
+    return np.all(A==A.T)
+
+
